e3e34482f3832bf6ba73913e7d0877d06537ea7c
thaovonguyen <125102310+thaovonguyen@users.noreply.github.com>
2023-11-27 20:08:35 +0700

    Two function

--- sql/function.sql
+++ sql/function.sql
@@ -17,16 +17,37 @@ BEGIN
         SET MESSAGE_TEXT = 'Chưa có dữ liệu cho thời gian này';
     ELSE
         -- Nếu điều kiện đúng, thực hiện truy vấn tính tổng
-        SELECT SUM(fine) INTO total
+        SELECT COALESCE(SUM(fine), 0) INTO total
         FROM fine_invoice
         WHERE YEAR(fdate) = in_year AND MONTH(fdate) = in_month AND (fstatus = 'Đã thanh toán' OR fstatus = 'Đã gạch nợ');
-        IF total IS NULL THEN
-			SET total = 0;
-		END IF;
         RETURN total;
     END IF;
 END //
 
+CREATE FUNCTION import_cost(id INT)
+RETURNS INT
+READS SQL DATA
+BEGIN
+    DECLARE key_exists INT;
+    DECLARE result INT;
 
+    -- Check if the import exists
+    SELECT COUNT(*) INTO key_exists
+    FROM printing_import
+    WHERE iid = id;
+
+    IF key_exists = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Đợt nhập không tồn tại';
+    ELSE
+        -- Calculate the sum of costs
+        SELECT COALESCE(SUM(cost), 0) INTO result
+        FROM printing
+        INNER JOIN printing_import ON printing_import.iid = printing.iid
+        WHERE printing_import.iid = id;
+
+        RETURN result;
+    END IF;
+END //
 
 DELIMITER ;
